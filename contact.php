7dfce23e371f446aecb4288979813bd3a193cd68
Amani Soufiane <soufianeamani88@gmail.com>
2021-02-17 00:21:37 +0100

    Premier commit, Page accueil terminé.

---
+++ contact.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Le Barista</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"></head>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+<header>
+    <div class="bloc_img_header">
+        <img class="img_header" src="./img/Le Barista 2.png" alt="">
+    </div>
+    <nav> 
+        <ul>
+            <a href="">
+                <li>HOME</li>
+            </a>
+            <a href="">
+                <li>NOTRE ORIGINE</li>
+            </a>
+            <a href="">
+                <li>COMMANDE</li>
+            </a>
+            <a href="">
+                <li>RESERVATION</li>
+            </a>
+            <a href="">
+                <li>CONTACT</li>
+            </a>
+        </ul>
+    </nav>
+</header>
+<body>
+    <form action="contact.php" method="post">
+        <div class="form-group">
+            <label></label>
+        </div>
+    </form>
+</body>
+
+<footer>
+    <div id="footer_2">
+        <p>Réalisé par Soufiane Amani - Diego Iscaro - Aurélie Henno - Hafsa El Mokadem</p>
+    </div>
+    <script src="https://kit.fontawesome.com/e6fdd6518a.js" crossorigin="anonymous"></script>
+</footer>
+</html>
